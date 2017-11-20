80eaf3afe18a0a1ae2370544efd49f3017c15480
Tim Goes <Tim_Goes4@hotmail.nl>
2017-11-20 07:25:41 -0800

    First implementation of getting screenspace world coords

---
+++ BlackoutVR/Source/BlackoutVR/Public/VRCapture2D.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/SceneCapture2D.h"
+#include "VRCapture2D.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKOUTVR_API AVRCapture2D : public ASceneCapture2D
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable, Category="Screen")
+	void ScreenToSieWorld(FVector2D touchPosition, FVector& worldLocation, FVector& worldDirection);
+	
+	
+};
