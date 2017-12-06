f0d66967167da936ee85cf05817704a3699e900e
Tim Goes <Tim_Goes4@hotmail.nl>
2017-12-06 11:09:09 +0100

    Auto stash before merge of "develop" and "origin/develop"
    Possibly fixed touch widget interaction

---
+++ BlackoutVR/Source/BlackoutVR/Private/SpectatorWidgetInteraction.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SpectatorWidgetInteraction.h"
+
+/*USpectatorWidgetInteraction::FWidgetTraceResult USpectatorWidgetInteraction::PerformTrace() const
+{
+	return USpectatorWidgetInteraction::FWidgetTraceResult();
+}
+*/
---
+++ BlackoutVR/Source/BlackoutVR/Public/SpectatorWidgetInteraction.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetInteractionComponent.h"
+#include "Layout/WidgetPath.h"
+#include "SpectatorWidgetInteraction.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKOUTVR_API USpectatorWidgetInteraction : public UWidgetInteractionComponent
+{
+	GENERATED_BODY()
+
+
+protected:
+		//FWidgetTraceResult PerformTrace() const override;
+	
+	
+};
